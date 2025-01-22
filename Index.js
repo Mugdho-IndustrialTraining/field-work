e5b96c49a249c4cbb0f0ffb3ecc9659da6a17a98
Mugdho-IndustrialTraining <mukusutradhar728@gmail.com>
2025-01-23 04:22:39 +0600

    Update and rename Index.j to Index.js

--- Index.js
+++ Index.js
@@ -1,10 +1,10 @@
-// You can dynamically update the skill progress if needed
+s// You can dynamically update the skill progress if needed
 const skills = {
   "software-dev": 90,
   "python": 85,
   "html": 80,
   "css": 75,
-  "js": 70
+  "js": 70,
 };
 
 Object.keys(skills).forEach(skill => {
